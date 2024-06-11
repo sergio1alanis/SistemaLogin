fb45c654693e3dcefd7013f94cabc39d0d9faf13
sergio1alanis <sergio1alanis@hotmail.com>
2024-06-11 12:46:22 -0600

    Avances index.php, login.html, regsitro.html

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+<a href="https://dpw1-u2-ea-seaa.000webhostapp.com/">Pagina Resevaciones turisticas 000WebHost</a>
+|
+<a href="http://www.youtube.com/@sergioalanis8741">Youtube Serge</a>
+|
+<a href="cerrar.php">Cerrar</a><br>
+
+<h2>Inicio de la  aplicación</h2>
+<p>Estes es el inicio de la aplicación</p>
+</body>
+</html>
